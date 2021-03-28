705289421e58b1aeacc9d98148afccb0a014360a
Andrew King <pepsiotaku@gmail.com>
2021-03-28 11:51:57 -0400

    removed out, added to .gitignore

--- out/Commands/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-const vscode = require("vscode");
-const helloWorld = () => {
-    vscode.window.showInformationMessage("Hello from chalet-tools!");
-};
-exports.helloWorld = helloWorld;
-//# sourceMappingURL=HelloWorld.js.map
