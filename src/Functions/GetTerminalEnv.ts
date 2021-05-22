daacca4546705dd6fda33a6f4c5fb2d8cc2c212b
Andrew King <pepsiotaku@gmail.com>
2021-05-22 08:55:17 -0400

    fixed nasty environment variable bug

--- src/Functions/GetTerminalEnv.ts
+++ src/Functions/GetTerminalEnv.ts
@@ -2,19 +2,17 @@ import { workspace } from "vscode";
 import { Dictionary } from "../Types";
 import { VSCodePlatform } from "../Types/Enums";
 
-const rootEnv: Dictionary<string> = process.env as Dictionary<string>;
+const PATH_WIN = "Path";
+const PATH_UNIX = "PATH";
+
+const rootEnv: Dictionary<string> = JSON.parse(JSON.stringify(process.env)) as Dictionary<string>;
 
 export const getTerminalEnv = (platform: VSCodePlatform): Dictionary<string> => {
-    let out: Dictionary<string> = rootEnv;
+    let out: Dictionary<string> = JSON.parse(JSON.stringify(rootEnv));
 
-    /*
-    let inheritEnv: boolean = true;
     const workspaceConfig = workspace.getConfiguration("terminal");
     if (workspaceConfig["integrated"]) {
         const integratedTerminal: any = workspaceConfig["integrated"];
-        // if (integratedTerminal["inheritEnv"]) {
-        //     inheritEnv = integratedTerminal["inheritEnv"];
-        // }
         if (integratedTerminal["env"]) {
             const terminalEnv: any = integratedTerminal["env"];
             if (terminalEnv[platform]) {
@@ -28,7 +26,13 @@ export const getTerminalEnv = (platform: VSCodePlatform): Dictionary<string> => 
                             for (const match of matches) {
                                 if (match.length < 2) break;
 
-                                const env = process.env[match[1]];
+                                if (
+                                    (match[1] != PATH_WIN && platform === VSCodePlatform.Windows) ||
+                                    (match[1] != PATH_UNIX && platform !== VSCodePlatform.Windows)
+                                )
+                                    continue;
+
+                                const env = rootEnv[match[1]];
                                 if (env) {
                                     const re = new RegExp(match[0].replace("$", "\\$"), "g");
                                     outValue = outValue.replace(re, env.replace(/\\/g, "/"));
@@ -44,27 +48,28 @@ export const getTerminalEnv = (platform: VSCodePlatform): Dictionary<string> => 
         }
     }
 
-    if (inheritEnv) {
-        const PATH_WIN = "Path";
-        const PATH_UNIX = "PATH";
-        if (platform === VSCodePlatform.Windows) {
-            let path = process.env[PATH_WIN];
-            if (path) {
-                path = path.replace(/\\/g, "/");
-                if (out[PATH_WIN] && !out[PATH_WIN].includes(path)) {
-                    out[PATH_WIN] = `${out[PATH_WIN]};${path}`;
-                }
+    if (platform === VSCodePlatform.Windows) {
+        let path = rootEnv[PATH_WIN];
+        path = path.replace(/\\/g, "/");
+        if (path) {
+            if (out[PATH_WIN] && !out[PATH_WIN].includes(path)) {
+                out[PATH_WIN] += ";" + path;
             }
-        } else {
-            let path = process.env[PATH_UNIX];
-            if (path) {
-                if (out[PATH_UNIX] && !out[PATH_UNIX].includes(path)) {
-                    out[PATH_UNIX] = `${out[PATH_UNIX]}:${path}`;
-                }
+        }
+    } else {
+        let path = rootEnv[PATH_UNIX];
+        if (path) {
+            if (out[PATH_UNIX] && !out[PATH_UNIX].includes(path)) {
+                out[PATH_UNIX] += ":" + path;
             }
         }
     }
-    */
+
+    if (platform === VSCodePlatform.Windows) {
+        console.log(out[PATH_WIN]);
+    } else {
+        console.log(out[PATH_UNIX]);
+    }
 
     return out;
 };
